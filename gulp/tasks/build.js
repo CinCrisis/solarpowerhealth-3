0a028a89902a499eecd59bc56c3af102bf8df71d
Courtenay <nay.adams+github@gmail.com>
2019-09-28 15:39:43 -0700

    Completed deployment prep

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -9,9 +9,9 @@ browserSync = require('browser-sync').create();
 
 gulp.task('previewDist', function() {
 	  browserSync.init({
-    notify: false,
-    server: {
-      baseDir: "docs"
+		  notify: false,
+		  server: {
+	      baseDir: "docs"
     }
   });
 });
